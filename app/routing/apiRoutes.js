c791df41c46642fe3ea9b302b77c7eb724719951
Anson <anson.g.ng@gmail.com>
2019-06-11 12:13:44 -0700

    updated

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -19,25 +19,24 @@ module.exports = function(app, path) {
 		fs.readFile('app/data/friends.js', function (err, data) {
 			// Read existing friends to array
 		    var friends = JSON.parse(data);
-
-		    var bestMatch = req.body;
-		    var bestScore = 999;
 			
+		    var bestMatch = -1;
+		    var bestScore = 999;
+
 		    // Loop through the friends to find the best match
-		    for (var f in friends) {
+		    for (var i = 0; i < friends.length; i++) {
 		    	var currentScore = 0;
-		    	for (var i = 0; j < f['answers[]'].length; j++) {
-		    		distance += Math.abs((parseInt(req.body['answers[]'][i]) - parseInt(f['answers[]'][i])));
+		    	for (var j = 0; j < friends[i]['scores'].length; j++) {
+		    		currentScore += Math.abs((parseInt(req.body['scores[]'][j]) - parseInt(friends[i]['scores'][j])));
 				}
 
 				if(currentScore <= bestScore) {
 					bestScore = currentScore;
-					bestMatch = f;
+					bestMatch = i;
 		    	}
 		    }
-	
 			// Return results
-			res.send(bestMatch);
+			res.send(friends[bestMatch]);
 
 		});
 	});
